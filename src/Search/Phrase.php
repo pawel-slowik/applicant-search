b0f65580a096e6c28f3527f29701893e35536bdf
Paweł Słowik <pawel.slowik@zoho.com>
2021-11-05 19:27:42 +0100

    Validate search phrase length

--- src/Search/Phrase.php
+++ src/Search/Phrase.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace Recruitment\Search;
 
+use UnexpectedValueException;
+
 class Phrase
 {
+    private const MAX_LENGTH = 200;
     /**
      * @var string[]
      */
@@ -13,6 +16,9 @@ class Phrase
 
     public function __construct(string $phrase)
     {
+        if (!self::validate($phrase)) {
+            throw new UnexpectedValueException();
+        }
         // could do boolean expression / tag analysis here (or in a factory)
         $this->words = preg_split('/\s/', $phrase, \PREG_SPLIT_NO_EMPTY);
     }
@@ -23,5 +29,14 @@ class Phrase
     public function getWords(): array
     {
         return $this->words;
+    }
+
+    private static function validate($phrase): bool
+    {
+        if (mb_strlen($phrase, 'UTF-8') > self::MAX_LENGTH) {
+            return false;
+        }
+
+        return true;
     }
 }
