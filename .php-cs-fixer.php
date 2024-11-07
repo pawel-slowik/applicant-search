96934b9a827ea2d4f4de4b657cc5668ea56e8cc3
Paweł Słowik <pawel.slowik@zoho.com>
2024-11-07 17:08:45 +0100

    Update components

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -10,5 +10,6 @@ return (new Config())
         '@Symfony:risky' => true,
         'native_function_invocation' => false,
         'concat_space' => ['spacing' => 'one'],
+        'global_namespace_import' => true,
     ])
     ->setFinder(Finder::create()->in(__DIR__));
